8c754bf55a369941bc97b684efd35183051845c1
Ejike Chinecherem Victory <56884436+victoryejike@users.noreply.github.com>
2021-05-02 22:23:33 +0100

    Started styling of the sidebar component and changed some of the styling of the index.scss file

--- src/components/SideBarComponent/SideBar.js
+++ src/components/SideBarComponent/SideBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SideBar.scss";
 
 const SideBar = () => {
   return (
@@ -8,11 +9,22 @@ const SideBar = () => {
           <img alt="user stat display img" />
         </div>
         <div className="sideBar-user-detail">
-          <h3 className="user-name"> </h3>
-          <br></br>
-          <span className="username"> </span>
+          <h2 className="user-name"> Ejike Chinecherem Victory </h2>
+
+          <span className="username">victoryejike </span>
           <h4 className="user-designation"> </h4>
           <button className="user-editProfile-button">Edit Profile</button>
+          <div className="user-social-details">
+            <small> 2 followers</small>
+            <small> 2 following</small>
+            <small> 2 </small>
+          </div>
+        </div>
+        <div className="user-achievements">
+          <img alt="user-achievement" />
+        </div>
+        <div className="user-organization">
+          <img alt="user-organization" />
         </div>
       </div>
     </>
--- src/pages/UserStatPage.js
+++ src/pages/UserStatPage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Card from "../components/CardComponent/Card";
+import SideBar from "../components/SideBarComponent/SideBar";
+import "./UserStatPage.scss";
 
 const User = () => {
   return (
-    <>
+    <div className="user-display">
       <h1>Display User Stat here</h1>
+      <SideBar />
       <Card title="E-commerce" subtitle="forked from victorygithub" />
-    </>
+    </div>
   );
 };
 
