7dbf95a7d032a134455ca1583ee0b20590a4059b
Ejike Chinecherem Victory <56884436+victoryejike@users.noreply.github.com>
2021-05-14 04:31:31 +0100

    created a contribution card component for data using css grid

---
+++ src/components/ContributionCardComponent/ContributionCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./ContributionCard.scss";
+
+const ContributionCard = () => {
+  return (
+    <>
+      <div className="graph">
+        <ul className="months">months</ul>
+        <ul className="days">days</ul>
+        <ul className="squares"></ul>
+      </div>
+    </>
+  );
+};
+
+export default ContributionCard;
--- src/components/UserContributionComponent/UserContribution.js
+++ src/components/UserContributionComponent/UserContribution.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./UserContribution.scss";
 
+import ContributionCard from "../ContributionCardComponent/ContributionCard";
+
 const UserContribution = () => {
   return (
     <>
       <div className="user-activity">
         <div className="contribution-container">
-          <div className="repo-info contribution">
-            <p>210 Contributions in the last year</p>
-            <p>Contribution settings</p>
+          <div>
+            <div className="repo-info contribution">
+              <p>210 Contributions in the last year</p>
+              <p>Contribution settings</p>
+            </div>
+            <ContributionCard />
           </div>
           <aside>
             <button className="btn-year">2021</button>
